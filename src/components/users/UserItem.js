c0d9cd38120f3a0db04624d489861582836290b0
Michael AJAYI O <ajayimichael@hotmail.com>
2021-06-01 13:50:51 +0000

    Initial Commit

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "id",
+      login: "mojombo",
+      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo" 
+    };
+  }
+
   render() {
-    return <div>UserItem</div>;
+    return <div className="card text-center">
+      <img src={this.state.avatar_url} className="round-img" style={{ width: '60px' }}></img>
+      <h3>{this.state.login}</h3>
+
+      <div>
+        
+      </div>
+    </div>;
   }
 }
 
