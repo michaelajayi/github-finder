fd11fbd39ea9d8dbfd5aee5dfd02f55b3f1e2d94
Michael AJAYI O <ajayimichael@hotmail.com>
2021-06-01 13:11:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const numbers = [1, 2, 3, 4];
+    return (
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
+}
+
+export default App;
