9020808b90bb510abf8339552d4b389d305347e0
Michael AJAYI O <ajayimichael@hotmail.com>
2021-06-19 23:47:59 +0100

    Introduced React Hooks (useState, useEffect)

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "GitHub Finder",
-    icon: "fab fa-github",
-  };
+const Navbar = ({ icon, title}) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+    </nav>
+  );
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: "GitHub Finder",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: "id",
-      login: "mojombo",
-      avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo" 
-    };
-  }
-
-  render() {
-    return <div className="card text-center">
-      <img src={this.state.avatar_url} className="round-img" style={{ width: '60px' }}></img>
-      <h3>{this.state.login}</h3>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        className='round-img'
+        style={{ width: "60px" }}
+        alt=''
+      ></img>
+      <h3>{login}</h3>
 
       <div>
-        
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
       </div>
-    </div>;
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
