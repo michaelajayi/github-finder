9ab52ebe276cc62e97e22c9530266ac1eba90e6d
Michael AJAYI O <ajayimichael@ohotmail.com>
2021-06-24 23:35:15 +0100

    Moved repos & getRepos to App Level Context

--- src/App.js
+++ src/App.js
@@ -6,29 +6,13 @@ import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import axios from "axios";
 
 import GithubState from './context/github/GithubState';
 
 import "./App.css";
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-
-  // Get user repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort==created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    // Clear users from state
-    setRepos(res.data);
-    setLoading(false);
-  };
 
   // Set Alert
   const showAlert = (msg, type) => {
@@ -61,14 +45,7 @@ const App = () => {
               <Route
                 exact
                 path='/user/:login'
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUserRepos={getUserRepos}
-                    repos={repos}
-                    loading={loading}
-                  />
-                )}
+                component={User}
               />
             </Switch>
           </div>
